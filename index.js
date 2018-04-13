26c3947cc20c92f3350c2035efcacdc439cb55ca
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-13 14:43:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 function scuberGreetingForFeet(someValue) {
   //this is where we can use conditionals given our argument, someValue
   //don't forget to return whatever the result is!
+  if (someValue <= 400) {
+    return 'This one is on me!'
+  } else if (someValue > 2000 && someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
+    return 'No can do.'
+  }
 }
 
 function ternaryCheckCity() {
-  
+
 }
 
 function switchOnCharmFromTip() {
-  
+
 }
